bd1fdee0d8e081c0072b3428155c71cb96cd7555
Sarp Ozturk <sarp.ozturk@ozu.edu.tr>
2022-11-30 13:03:55 +0300

    Initial commit

---
+++ SwiftUIPortfolio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPortfolio
+//
+//  Created by Sarp  on 30.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIPortfolio/SwiftUIPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPortfolioApp.swift
+//  SwiftUIPortfolio
+//
+//  Created by Sarp  on 30.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
