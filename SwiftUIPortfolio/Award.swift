29fdc0e6f031ba488acbf3fa68acb38e22598174
Sarp Ozturk <sarp.ozturk@ozu.edu.tr>
2022-12-03 17:59:06 +0300

    create Award struct

---
+++ SwiftUIPortfolio/Award.swift
@@ -0,0 +1,24 @@
+//
+//  Award.swift
+//  SwiftUIPortfolio
+//
+//  Created by Sarp  on 3.12.2022.
+//
+
+import Foundation
+
+struct Award: Identifiable, Decodable {
+    var id: String {
+        name
+    }
+    
+    let name: String
+    let description: String
+    let color: String
+    let criterion: String
+    let value: Int
+    let image: String
+    
+    static let allAwards: [Award] = Bundle.main.decode([Award].self, from: "Awards.json")
+    static let example = allAwards[0]
+}
---
+++ SwiftUIPortfolio/Bundle-Decodable.swift
@@ -0,0 +1,31 @@
+//
+//  Bundle-Decodable.swift
+//  SwiftUIPortfolio
+//
+//  Created by Sarp  on 3.12.2022.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode<T: Decodable>(_ type: T.Type, from file: String, dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate, keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys) -> T {
+        guard let url = Bundle.main.url(forResource: file, withExtension: nil) else {
+            fatalError()
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError()
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = dateDecodingStrategy
+        decoder.keyDecodingStrategy = keyDecodingStrategy
+        
+        do {
+            return try decoder.decode(type, from: data)
+        }
+        catch {
+            fatalError()
+        }
+    }
+}
