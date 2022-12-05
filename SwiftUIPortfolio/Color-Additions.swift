37ebc9c79165c6e7f80bb19a485fa86d9f8fb1ff
Sarp Ozturk <sarp.ozturk@ozu.edu.tr>
2022-12-05 17:31:13 +0300

    add projects and items to HomeView

---
+++ SwiftUIPortfolio/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  SwiftUIPortfolio
+//
+//  Created by Sarp  on 5.12.2022.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
