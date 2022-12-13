ccdf80f15d0a5dc9b376cf177bb29f571d07ab8f
Sarp Ozturk <sarp.ozturk@ozu.edu.tr>
2022-12-13 17:01:56 +0300

    refactor HomeView

---
+++ SwiftUIPortfolio/ProjectSummaryView.swift
@@ -0,0 +1,29 @@
+//
+//  ProjectSummaryView.swift
+//  SwiftUIPortfolio
+//
+//  Created by Sarp  on 13.12.2022.
+//
+
+import SwiftUI
+
+struct ProjectSummaryView: View {
+    @ObservedObject var project: Project
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("\(project.projectItems.count) items")
+                .font(.caption)
+                .foregroundColor(Color.secondary)
+            
+            Text(project.projectTitle)
+                .font(.title2)
+            ProgressView(value: project.completionAmount)
+                .accentColor(Color(project.projectColor))
+        }
+        .padding()
+        .background(Color.secondarySystemGroupedBackground)
+        .cornerRadius(10)
+        .shadow(color: .black.opacity(0.2), radius: 5)
+    }
+}
